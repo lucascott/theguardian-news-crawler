5dacbe9cd50d245557fc29de63f4bfbfe7da6183
Luca Scotton <luca.scotton@gofore.com>
2019-03-05 13:33:11 +0100

    Initial commit

---
+++ deguardianboi.py
@@ -0,0 +1,43 @@
+from pprint import pprint
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class DeGuardianBoi():
+
+    def __init__(self):
+        self.corpus = {}
+
+    @staticmethod
+    def get(url):
+        try:
+            return requests.get(url)
+        except requests.exceptions.ConnectionError:
+            pass
+
+    def _get_article(self, url):
+        response = self.get(url)
+        return BeautifulSoup(response.content, features="html.parser")
+
+    def fetch_json(self, url):
+        response = self.get(url)
+        return response.json()
+
+    def fetch_article(self, url):
+        soup = self._get_article(url)
+        if soup is None:
+            return None
+        try:
+            p_list = soup.find('div', {'itemprop': 'articleBody'}).find_all('p', recursive=False)
+            article = [p.text for p in p_list if p.text]
+            return article
+        except:
+            return False
+
+
+
+if __name__ == '__main__':
+    bb = DeGuardianBoi()
+    bb.fetch_article(
+        'https://www.theguardian.com/technology/2019/feb/24/are-you-being-scanned-how-facial-recognition-technology-follows-you-even-as-you-shop')
---
+++ main.py
@@ -0,0 +1,54 @@
+import json
+
+from deguardianboi import DeGuardianBoi
+
+'''
+Create a file named private.py and inside declare a var api_key = <your_api_key>
+'''
+import private
+
+
+base_url = 'https://content.guardianapis.com/search?'
+api_key = private.api_key
+categories = ['football', 'technology', 'politics']
+incremental_label = 'page'
+key_label = 'api-key'
+query_label = 'q'
+
+total_articles = 1000
+
+output_path = './data/articles_'
+
+
+def concat_params(params):
+    return '&'.join([(str(key) + '=' + str(val)).lower() for key, val in params.items()])
+
+
+def get_n_pages(total_articles, n_categories, articles_per_page=10):
+    return max(round((total_articles / n_categories) / articles_per_page), 1)
+
+
+dgb = DeGuardianBoi()
+data = {}
+for c in categories:
+    data[c] = {}
+    for i in range(1, 1 + get_n_pages(total_articles, len(categories))):
+        url = base_url + concat_params({
+            key_label: api_key,
+            query_label: c,
+            incremental_label: i
+        })
+        res = dgb.fetch_json(url)
+        for r in res['response']['results']:
+            url = r['webUrl']
+            print(f'Fetching: {url}')
+            corpus = dgb.fetch_article(url)
+            if corpus is not False:
+                r['corpus'] = corpus
+                data[c][r['id']] = r
+            else:
+                print(f'The corpus couldn\'t be loaded. The article {url} has been skipped.')
+    comp_path = output_path + c + '.json'
+    with open(comp_path, 'w') as fout:
+        json.dump(data[c], fout)
+        print(f"Dumped {comp_path} successfully.")
