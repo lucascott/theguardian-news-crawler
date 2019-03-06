4e40015099187384424cb67bb16c4d0dc6e798df
Luca Scotton <luca.scotton@gofore.com>
2019-03-06 09:57:18 +0100

    update process bar

--- main.py
+++ main.py
@@ -34,25 +34,28 @@ def get_n_pages(total_articles, articles_per_page=10):
 
 dgb = DeGuardianBoi()
 data = {}
-for c in categories:
-    data[c] = {}
-    for i in tqdm(range(1, 1 + get_n_pages(articles_per_cat))):
-        url = base_url + concat_params({
-            key_label: api_key,
-            query_label: c,
-            incremental_label: i
-        })
-        res = dgb.fetch_json(url)
-        for r in res['response']['results']:
-            url = r['webUrl']
-            # print(f'Fetching: {url}')
-            corpus = dgb.fetch_article(url)
-            if corpus is not False:
-                r['corpus'] = corpus
-                data[c][r['id']] = r
-            else:
-                print(f'The corpus could not be loaded. The article {url} has been skipped.')
-    comp_path = output_path + c + '.json'
-    with open(comp_path, 'w') as fout:
-        json.dump(data[c], fout)
-        print(f"Dumped {comp_path} successfully.")
+with tqdm(total=articles_per_cat * len(categories), desc=categories[0]) as pbar:
+    for c in categories:
+        data[c] = {}
+        pbar.desc = c
+        for i in range(1, 1 + get_n_pages(articles_per_cat)):
+            url = base_url + concat_params({
+                key_label: api_key,
+                query_label: c,
+                incremental_label: i
+            })
+            res = dgb.fetch_json(url)
+            for r in res['response']['results']:
+                url = r['webUrl']
+                # print(f'Fetching: {url}')
+                corpus = dgb.fetch_article(url)
+                if corpus is not False:
+                    r['corpus'] = corpus
+                    data[c][r['id']] = r
+                else:
+                    print(f'The corpus could not be loaded. The article {url} has been skipped.')
+                pbar.update(1)
+        comp_path = output_path + c + '.json'
+        with open(comp_path, 'w') as fout:
+            json.dump(data[c], fout)
+            print(f"\nDumped {comp_path} successfully.")
