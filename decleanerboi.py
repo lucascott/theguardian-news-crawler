4d44a0cfd6821a902fc6b87f79f301046804b649
Luca Scotton <lucascott1996@gmail.com>
2019-04-26 02:12:01 +0200

    spacy preprocess for performance + user recommendation

--- decleanerboi.py
+++ decleanerboi.py
@@ -1,4 +1,5 @@
 import nltk
+import spacy
 from nltk import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 
@@ -7,17 +8,43 @@ class DeCleanerBoi:
     def __init__(self):
         nltk.download('punkt')
         nltk.download('stopwords')
+        self.stemmer = nltk.SnowballStemmer('english')
+        self.nlp = spacy.load("en_core_web_sm")
+        self.nlp.remove_pipe('tagger')
+        self.nlp.remove_pipe('parser')
+        self.nlp.remove_pipe('ner')
 
-    @staticmethod
-    def clean_article(art):
+    def clean_article(self, art):
+
         tokens = []
         for sent in sent_tokenize(art):
             for word in word_tokenize(sent):
                 word = word.lower()
+                tokens = list(map(lambda w: self.stemmer.stem(w), tokens))
+                tokens = [w for w in tokens if w.isalpha()]
                 if len(word) < 2 or word in stopwords.words('english'):
                     continue
                 tokens.append(word)
         return tokens
+
+    def SPACYPipeline(self, doc):
+        """
+        Transform the corpus with Spacy library
+        :param doc:
+        :return: list of lists of tokens (strings) e.g. [['hello', ''world'],['how', 'are', 'you']]
+        """
+
+        tokens = self.nlp(doc)
+
+        new_tokens = []
+        for w in tokens:
+            # if it's not a stop word or punctuation mark, add it to our article!
+            lemma = w.lemma_  # lemmatized version of the word
+            if lemma != '\n' and len(lemma) > 2 and (
+                    not w.is_stop or lemma == 'not') and not w.is_punct and not w.like_num:
+                new_tokens.append(lemma)
+
+        return new_tokens
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -3,33 +3,39 @@ import json
 import multiprocessing
 import os
 import pickle
-from collections import namedtuple
+import random
 
+import numpy as np
 from gensim.models import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+from sklearn.metrics.pairwise import linear_kernel
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 from decleanerboi import DeCleanerBoi
 
+# %%
 filepath = './data/'
 prefix = 'articles_'
 docs_path = filepath + 'docs.pickle'
 document_list = glob.glob(filepath + prefix + '*.json')
-CleanedDocument = namedtuple('CleanedDocument', 'words tags')
 docs = []
+categories = {'film', 'technology', 'travel', 'food', 'business', 'fashion', 'education', 'artanddesign',
+              'football', 'games'}
+
 # %%
 dcb = DeCleanerBoi()
 
 for name in document_list:
     category = os.path.basename(name).split('_')[1].split('.')[0]
     with open(name, 'r') as fin:
-
         documents = json.load(fin)
         for _, doc in tqdm(documents.items(), desc=category, unit='articles'):
-            words = dcb.clean_article(' '.join(doc['corpus']))
-            docs.append(CleanedDocument(words, category))
+            words = dcb.SPACYPipeline(' '.join(doc['corpus']))
+            docs.append(TaggedDocument(words=words, tags=category))
 
 with open(docs_path, 'wb') as fout:
     pickle.dump(docs, fout)
@@ -41,24 +47,112 @@ with open(docs_path, 'rb') as fin:
     docs = pickle.load(fin)
     print(f"{docs_path} loaded successfully.")
 ds_train, ds_test = train_test_split(docs, test_size=0.25)
-model_dbow = Doc2Vec(vector_size=100, window=300, min_count=2, workers=multiprocessing.cpu_count())
-model_dbow.build_vocab(ds_train)
+model = Doc2Vec(vector_size=100, window=300, min_count=2, workers=multiprocessing.cpu_count())
+model.build_vocab(ds_train)
+print('Training time!')
 for epoch in tqdm(range(30)):
-    model_dbow.train(ds_train, total_examples=len(ds_train), epochs=1)
-    model_dbow.alpha -= 0.002
-    model_dbow.min_alpha = model_dbow.alpha
+    model.train(ds_train, total_examples=len(ds_train), epochs=1)
+    model.alpha -= 0.002
+    model.min_alpha = model.alpha
 
 
+# %% Learning from matrix representation of each of the documents
 def vec_for_learning(model, sents):
     return zip(*[(model.infer_vector(doc.words, epochs=20), doc.tags) for doc in sents])
 
 
 print('Train/test split...')
-X_train, y_train, = vec_for_learning(model_dbow, ds_train)
-X_test, y_test = vec_for_learning(model_dbow, ds_test)
+X_train, y_train, = vec_for_learning(model, ds_train)
+X_test, y_test = vec_for_learning(model, ds_test)
 print('Training model...')
 logreg = LogisticRegression(n_jobs=multiprocessing.cpu_count(), C=1., solver='lbfgs', multi_class='multinomial')
 logreg.fit(X_train, y_train)
 print('Predicting topics...')
 y_pred = logreg.predict(X_test)
 print(classification_report(y_test, y_pred))
+
+# %%
+
+tfidf = TfidfVectorizer(analyzer='word')
+
+X_train_tfidf = tfidf.fit_transform(*zip(*[(' '.join(d.words), d.tags) for d in ds_train]))
+X_test_tfidf = tfidf.transform([' '.join(d.words) for d in ds_test])
+logreg = LogisticRegression(n_jobs=multiprocessing.cpu_count(), C=1., solver='lbfgs', multi_class='multinomial')
+logreg.fit(X_train_tfidf, y_train)
+
+print('Predicting topics...')
+y_pred = logreg.predict(X_test_tfidf)
+print(classification_report([d.tags for d in ds_test], y_pred))
+
+# %%
+
+count = CountVectorizer(analyzer='word')
+
+X_train_count = count.fit_transform(*zip(*[(' '.join(d.words), d.tags) for d in ds_train]))
+X_test_count = count.transform([' '.join(d.words) for d in ds_test])
+logreg = LogisticRegression(n_jobs=multiprocessing.cpu_count(), C=1., solver='lbfgs', multi_class='multinomial')
+logreg.fit(X_train_count, y_train)
+
+print('Predicting topics...')
+y_pred = logreg.predict(X_test_count)
+print(classification_report([d.tags for d in ds_test], y_pred))
+
+user_profile_size = X_train_tfidf.shape[1]
+
+user_query = X_test_tfidf[0]
+
+cosine_similarities = linear_kernel(user_query, X_train_tfidf).flatten()
+related_docs_indices = cosine_similarities.argsort()[:-6:-1]
+
+print(cosine_similarities[related_docs_indices])
+print([(ds_train[i].tags, ds_train[i].words[:10]) for i in related_docs_indices.tolist()])
+
+# %%
+
+print('Initializing users...')
+users = {}
+for c in categories:
+    users[c] = np.zeros(user_profile_size)
+print('Users initialized.')
+
+
+def tfidf_predict(query, user, user_perc):
+    query_vector = tfidf.transform([query])
+    query_vector = user * (user_perc) + query_vector * (1 - user_perc)
+    cosine_similarities = linear_kernel(query_vector, X_train_tfidf).flatten()
+    related_docs_indices = cosine_similarities.argsort()[:-6:-1]
+    # print([(ds_train[i].tags, ds_train[i].words[:10]) for i in related_docs_indices.tolist()])
+    return related_docs_indices
+
+
+def q_to_res(query, user, user_perc):
+    indexes = tfidf_predict(query, user, user_perc)
+    for ind in indexes:
+        print(ind, ds_train[ind].tags.upper(), ' '.join(ds_train[ind].words[:30]))
+    return indexes
+
+
+gamma = 0.6
+user_perc = 0.3
+for i in range(X_test_tfidf.shape[0] - 10):
+    user_query = ' '.join(ds_test[i].words)
+    category = ds_test[i].tags
+    print("QUERY: ", category.upper(), user_query[:200], '\n')
+    user = users[category]
+    indexes = q_to_res(user_query, user, user_perc)
+    chosen_result = random.choice(indexes)
+    disc_res = gamma * (X_train_tfidf[chosen_result] - user)
+    user = user + disc_res
+    users[category] = user
+    # print(np.mean(X_train_tfidf[chosen_result]))
+    # print(np.mean(user))
+    print('-' * 30)
+
+# %%
+user_doc = ds_test[-2]
+user_query = ' '.join(user_doc.words)
+print("QUERY: ", user_doc.tags.upper(), user_query[:200], '\n')
+for c in categories:
+    print('User who likes: ', c.upper())
+    q_to_res(user_query, users[c], user_perc)
+    print('-' * 30)
